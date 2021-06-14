454d27052dfaad79092dcf1efebdc8eda7e1b6e4
howard <howard67890@gmail.com>
2021-06-14 12:06:02 +0800

    add delete and notice

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,6 @@
 class StoriesController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_story, only: [:edit, :update, :destory]
+  before_action :find_story, only: [:edit, :update, :destroy]
 
   def index
     @stories = current_user.stories.order(created_at: :desc)
@@ -29,6 +29,11 @@ class StoriesController < ApplicationController
     else
       render :edit
     end
+  end
+  
+  def destroy
+    @story.destroy
+    redirect_to stories_path, notice: '故事已刪除'
   end
 
   private
