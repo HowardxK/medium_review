df0c3207bb438a1ea1c746102d8134b1af326077
howard <howard67890@gmail.com>
2021-06-23 10:18:29 +0800

    Follow 功能2

--- app/frontend/controllers/user_controller.js
+++ app/frontend/controllers/user_controller.js
@@ -8,10 +8,19 @@ export default class extends Controller {
     event.preventDefault()
 
     let user = this.followButtonTarget.dataset.user
+    let button = this.followButtonTarget
 
     // /users/:id/follow
     axios.post(`/users/${user}/follow`)
          .then(function(response) {
+           let status = response.data.status
+           switch(status) {
+             case 'sign_in_first':
+               alert('你必須先登入')
+               break
+             default:
+               button.innerHTML = status
+           }
            console.log(response.data)
          })
          .catch(function(error) {
